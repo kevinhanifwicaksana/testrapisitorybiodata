783598415f08b20fd41e1a5ac5290dd2f9fd6348
kevinhanifwicaksana <71705941+kevinhanifwicaksana@users.noreply.github.com>
2020-09-23 20:13:34 +0700

    project pertama

---
+++ biodata.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+int main ()
+{
+	system ("color A");
+	
+	printf ("\t BIODATA \n\n");
+	printf ("NAMA			:KEVIN HANIF WICAKSANA\n");
+	printf ("NPM			:18081010130\n");
+	printf ("alamat			:kab,nganjuk.\n			 kec,prambon.\n			 ds,baleturi.\n");
+	printf ("universitas 		:UPNVJT\n");
+	printf ("fakultas		:FIK\n");
+	printf ("prodi			:teknik informatika\n");
+	printf ("kelas			:pemprograman lanjut A\n");
+	
+	getch();
+	
+}
